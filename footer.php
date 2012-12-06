ee380f3c3869de072c4b55ef657a160ed1408469
Vilmos Ioo <cool.villi@gmail.com>
2012-12-06 20:29:58 +0000

    Cleaned up code and organised it
    Functions, header and footer files.

--- footer.php
+++ footer.php
@@ -1,32 +1,33 @@
 		<footer role="contentinfo">
-		<div class='container'>
-			<?php dynamic_sidebar('H_footer'); ?>
-			<div class='clear'></div>
-			<hr/>
-			<div class='fright'>
-				<?php wp_nav_menu( array('menu' => 'H_main', 'container' => false, )); ?>
+			<div class='container clearfix'>
+				<?php dynamic_sidebar('Footer'); ?>
+				<div class='clear'></div>
+				<hr/>
+				<div class='fright'>
+					<?php wp_nav_menu( array('menu' => 'H_main', 'container' => false, )); ?>
+				</div>
+				<p>Copyright Vilmos Ioo 2012</p>
 			</div>
-			<p>Copyright Vilmos Ioo 2012</p>
-		</div>
 		</footer>
 
-		<script defer src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
-		<script defer src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
-		<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
-		<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/twitter.json?callback=twitterCallback2&count=3"></script>
-
+		<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php echo THEME_PATH; ?>/js/lib/jquery-latest.min.js"%3E%3C/script%3E'))</script>
+		<script src="<?php echo THEME_PATH; ?>/js/lib/modernizr2.6.1.min.js" type="text/javascript" charset="utf-8"></script>
+		<script defer src="<?php echo THEME_PATH; ?>/js/script.js"></script>
 		<script> // Change UA-XXXXX-X to be your site's ID
-		window._gaq = [['_setAccount','UAXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];
-		Modernizr.load({
-		  load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
-		});
+			window._gaq = [['_setAccount','UAXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];
+			Modernizr.load({
+			  load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
+			});
 		</script>
 		
 		<!--[if lt IE 7 ]>
 		  <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
 		  <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
 		<![endif]-->
-		
+		<!--[if lte IE 8]>
+  			<script src="<?php echo THEME_PATH; ?>/js/lib/selectivizr-min.js" type="text/javascript" charset="utf-8"></script>
+		<![endif]-->
+
 		<?php wp_footer(); ?>
 	
 	</body>
--- functions.php
+++ functions.php
@@ -1,68 +1,13 @@
 <?php
+// ========================
+// define constants
+// ========================
+define( 'THEME_PATH', get_bloginfo( 'stylesheet_directory' ) );
+define( 'HOME_URL', home_url() );
 
 // ========================
-// = nice formated arrays =
-// ========================
-if( !function_exists( 'print_pre' ) ) {
-	function print_pre( $s ) {
-		echo '<pre>';
-		print_r( $s );
-		echo '</pre>';
-	}
-}
-
-// ========================
-// = get a custom length excerpt
-// ========================
-if( !function_exists( 'custom_excerpt' ) ) {
-	function custom_excerpt($s, $length){
-		$temp = substr(strip_tags($s), 0, $length);
-		if(strlen(strip_tags( $s ) ) > $length) $temp .= "&#0133;";
-		return $temp; 
-	}
-}
-
-// ========================
-// = Removes attached image sizes 
-// ========================
-add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
-add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10 );
-add_filter( 'the_content', 'remove_thumbnail_dimensions', 10 );
-function remove_thumbnail_dimensions( $html ) {
-	$html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
-	return $html;
-}
-
-// Custom CSS for the login page
-function LoginCSS() {
-	echo '<link rel="stylesheet" type="text/css" href="'.get_bloginfo('template_directory').'/wp-login.css"/>';
-}
-add_action('login_head', 'LoginCSS');
-
-// Customise the footer in admin area
-function remove_footer_admin () {
-	echo get_avatar('youremail@gmail.com' , '40' );
-	echo 'Theme designed and developed by <a href="#" target="_blank">[Your name]</a>';
-}
-add_filter('admin_footer_text', 'remove_footer_admin');
-
-// Custom CSS for the whole admin area (use to customise the theme options page)
-// Create wp-admin.css in your theme folder
-function AdminCSS() {
-    	echo '<link rel="stylesheet" type="text/css" href="'.get_bloginfo('template_directory').'/wp-admin.css"/>';
-}
-add_action('admin_head', 'AdminCSS');
-
-// Stop images getting wrapped up in p tags when they get dumped out with the_content() for easier theme styling
-function filter_ptags_on_images($content){
-	return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
-}
-add_filter('the_content', 'filter_ptags_on_images');
-
-// set up the theme options page
-get_template_part( 'theme-options-page' ); 
-
 // a smart way to get the post thumbnail
+// ========================
 function post_thumbdail($full){
 	if ( has_post_thumbnail() ) { 
 		echo "<aside><a href='".get_permalink()."' title='".get_the_title()."' rel='canonical'>";
@@ -90,81 +35,156 @@ function post_thumbdail($full){
 	}
 }
 
-//remove inline width and height added to images
+// ========================
+// = nice formated arrays =
+// ========================
+if( !function_exists( 'print_pre' ) ) {
+	function print_pre( $s ) {
+		echo '<pre>';
+		print_r( $s );
+		echo '</pre>';
+	}
+}
+
+// ========================
+// = get a custom length excerpt
+// ========================
+if( !function_exists( 'custom_excerpt' ) ) {
+	function custom_excerpt($s, $length){
+		$temp = substr(strip_tags($s), 0, $length);
+		if(strlen(strip_tags( $s ) ) > $length) $temp .= "&#0133;";
+		return $temp; 
+	}
+}
+
+// ========================
+// = removes attached image sizes 
+// ========================
 add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
 add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10 );
-
-// Removes attached image sizes as well
 add_filter( 'the_content', 'remove_thumbnail_dimensions', 10 );
 function remove_thumbnail_dimensions( $html ) {
 	$html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
 	return $html;
 }
 
-if ( function_exists('register_sidebar') ) {
-	register_sidebar(array(
-		'name' => 'H_main',
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<h3>',
-		'after_title' => '</h3>',
-	));
-	register_sidebar(array(
-		'name' => 'H_footer',
-		'before_widget' => '<div id="%1$s" class="widget grid-3 %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<h4>',
-		'after_title' => '</h4>',
-	));
+// ========================
+// = stop images getting wrapped up in p tags when they get dumped out with the_content() for easier theme styling
+// ========================
+function filter_ptags_on_images($content){
+	return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+add_filter('the_content', 'filter_ptags_on_images');
+add_action( 'after_setup_theme', 'hyperion_setup' );
+
+if ( !function_exists('hyperion_setup') ){
+	function hyperion_setup(){
+		if ( ! isset( $content_width ) ) $content_width = 1040;
+		add_theme_support( 'automatic-feed-links' );
+		add_theme_support( 'post-thumbnails' );
+		set_post_thumbnail_size( 150, 150, true );
+		add_theme_support( 'menus' );
+		add_theme_support( 'custom-header' );
+		add_custom_background();
+		add_editor_style();
+		hyperion_sidebars();
+		hyperion_customise_admin_area();
+		hyperion_removeBlogSlug();
+		get_template_part( 'theme-options-page' ); 
+
+		register_post_type('custom-post', array(
+			'label' => __('Custom post'),
+			'singular_label' => __('Custom post'),
+			'public' => true,
+			'show_ui' => true,
+			'capability_type' => 'post',
+			'hierarchical' => false,
+			'has_archive' => 'custom-post-index',
+			'supports' => array('title', 'editor', 'author', 'thumbnail', 'custom-fields'),
+		));
+	}
 }
 
-function wpe_excerptlength_news($length) {
-    return 15;
+// ========================
+// = create custom shortcodes
+// ========================
+function hyperion_shortcode( $atts, $content = null ) {
+	extract(shortcode_atts(array('attribute' => 'default_value'), $atts));
+	return "<div $attribute>".do_shortcode($content)."</div>";
+}
+add_shortcode('shortcode', 'hyperion_shortcode');
+
+// ========================
+// = remove blog/
+// ========================
+if ( !function_exists('hyperion_removeBlogSlug') ) {
+	function hyperion_removeBlogSlug(){
+		add_filter("pre_update_option_category_base","remove_blog_slug");
+		add_filter("pre_update_option_tag_base","remove_blog_slug");
+		add_filter("pre_update_option_permalink_structure","remove_blog_slug");
+
+		/* just check if the current structure begins with /blog/ remove that and return the stripped structure */
+		function remove_blog_slug($tag_cat_permalink){
+
+			if(!preg_match("/^\/blog\//",$tag_cat_permalink))
+			return $tag_cat_permalink;
+
+			$new_permalink=preg_replace ("/^\/blog\//","/",$tag_cat_permalink );
+			return $new_permalink;
+		}
+	}
 }
 
-function wpe_excerptlength_teaser($length) {
-    return 35;
-}
-function wpe_excerptlength_index($length) {
-    return 160;
-}
-function wpe_excerptmore($more) {
-    return '...<a href="'. get_permalink().'">Read More ></a>';
+// ========================
+// = register sidebars
+// ========================
+if ( !function_exists('hyperion_sidebars') ) {
+	function hyperion_sidebars(){
+		if ( function_exists('register_sidebar') ) {
+			register_sidebar(array(
+				'name' => 'Main',
+				'before_widget' => '<div id="%1$s" class="widget %2$s">',
+				'after_widget' => '</div>',
+				'before_title' => '<h3>',
+				'after_title' => '</h3>',
+			));
+			register_sidebar(array(
+				'name' => 'Footer',
+				'before_widget' => '<div id="%1$s" class="widget grid-3 %2$s">',
+				'after_widget' => '</div>',
+				'before_title' => '<h4>',
+				'after_title' => '</h4>',
+			));
+		}
+	}
 }
 
-function wpe_excerpt($length_callback='', $more_callback='') {
-    global $post;
-    if(function_exists($length_callback)){
-        add_filter('excerpt_length', $length_callback);
-    }
-    if(function_exists($more_callback)){
-        add_filter('excerpt_more', $more_callback);
-    }
-    $output = get_the_excerpt();
-    $output = apply_filters('wptexturize', $output);
-    $output = apply_filters('convert_chars', $output);
-    $output = '<p>'.$output.'</p>';
-    echo $output;
-}
+// ========================
+// = customise thwe dashboard and login area
+// ========================
 
-if ( ! isset( $content_width ) ) $content_width = 1040;
-add_theme_support( 'automatic-feed-links' );
-add_theme_support( 'post-thumbnails' );
-set_post_thumbnail_size( 150, 150, true );
-add_theme_support( 'menus' );
-add_theme_support( 'custom-header' );
-add_custom_background();
-add_editor_style();
- 
-register_post_type('custom-post', array(
-	'label' => __('Custom post'),
-	'singular_label' => __('Custom post'),
-	'public' => true,
-	'show_ui' => true,
-	'capability_type' => 'post',
-	'hierarchical' => false,
-	'has_archive' => 'custom-post-index',
-	'supports' => array('title', 'editor', 'author', 'thumbnail', 'custom-fields'),
-));
+if ( !function_exists('hyperion_customise_admin_area') ) {
+	function hyperion_customise_admin_area(){
+		// Custom CSS for the login page
+		function LoginCSS() {
+			echo '<link rel="stylesheet" type="text/css" href="'.get_bloginfo('template_directory').'/wp-login.css"/>';
+		}
+		add_action('login_head', 'LoginCSS');
+
+		// Customise the footer in admin area
+		function remove_footer_admin () {
+			echo get_avatar('youremail@gmail.com' , '40' );
+			echo 'Theme designed and developed by <a href="#" target="_blank">[Your name]</a>';
+		}
+		add_filter('admin_footer_text', 'remove_footer_admin');
+
+		// Custom CSS for the whole admin area (use to customise the theme options page)
+		// Create wp-admin.css in your theme folder
+		function AdminCSS() {
+		    	echo '<link rel="stylesheet" type="text/css" href="'.get_bloginfo('template_directory').'/wp-admin.css"/>';
+		}
+		add_action('admin_head', 'AdminCSS');
+	}
+}
 
 ?>
